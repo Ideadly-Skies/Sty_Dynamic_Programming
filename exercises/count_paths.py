5b436d3a89b09d9cf7ed0aba0a32e14ac3700b51
Ideadly-Skies <Obie.kal22@gmail.com>
2025-03-14 14:12:45 +0700

    feat: implemented max_path_sum using recursion (DP)

--- exercises/count_paths.py
+++ exercises/count_paths.py
@@ -1,22 +1,38 @@
 def count_paths(grid):
-    return _count_paths(grid, 0, 0)
+    ans = _count_paths(grid, 0, 0)
+    return ans
 
 def _count_paths(grid, r, c, memo={}):
-    if r == len(grid) or c == len(grid[0]) or grid[r][c] == 'X':
-        # don't count path that leads to out of bound 
-        return 0
-
+    # return memo in dictionary
     if (r, c) in memo:
         return memo[(r, c)]
 
-    if r == len(grid)-1 and c == len(grid[0])-1:
-        # if i am already in the bottom right corner,
-        # there's just one way to reach the bottom
-        # right corner 
-        return 1
+    # handle the case when you step out of bounds
+    if r == len(grid) or c == len(grid[0]) or grid[r][c] == 'X':
+        return 0 # don't want to count any paths that lead out of bounds
 
-    # return total number of ways going down and right 
+    # base case
+    if r == len(grid)-1 and c == len(grid[0])-1:
+        return 1 # count this as 1
+
+    # recursive case
     down_count = _count_paths(grid, r+1, c, memo)
     right_count = _count_paths(grid, r, c+1, memo)
-    memo[(r, c)] = down_count + right_count    
-    return down_count + right_count
+
+    # place computation in memo
+    memo[(r, c)] = down_count + right_count
+    return memo[(r, c)] 
+
+if __name__ == "__main__":
+    grid = [
+        ["O", "O"],
+        ["O", "O"]
+    ] 
+
+    grid = [
+        ["O", "O", "X"],
+        ["O", "O", "O"],
+        ["O", "O", "O"],
+    ] 
+
+    print(count_paths(grid))
---
+++ exercises/max_path_sum.py
@@ -0,0 +1,42 @@
+def max_path_sum(grid):
+    # memo to store computed result 
+    memo = {} 
+
+    return _max_path_sum(grid, 0, 0, memo) 
+
+def _max_path_sum(grid, r, c, memo):
+    # return value of memo in pos 
+    if (r, c) in memo:
+        return memo[(r, c)]
+
+    # check if entry is out of bounds
+    if r == len(grid) or c == len(grid[0]):
+        return float("-inf")
+
+    # base case 
+    if r == len(grid)-1 and c == len(grid[0])-1:
+        return grid[r][c] 
+        
+    # recursive case
+    left_sum = _max_path_sum(grid, r+1, c, memo)
+    right_sum = _max_path_sum(grid, r, c+1, memo) 
+
+    # return max from left_sum and right_sum
+    memo[(r, c)] = grid[r][c] + max(left_sum, right_sum)
+    return memo[(r, c)] 
+
+if __name__ == "__main__":
+    grid = [
+        [1, 3, 12],
+        [5, 1, 1],
+        [3, 6, 1],
+    ]
+    
+    print(max_path_sum(grid)) # -> 18
+
+    grid = [
+        [1, 2, 8,  1],
+        [3, 1, 12, 10],
+        [4, 0, 6,  3],
+    ]
+max_path_sum(grid) # -> 36
